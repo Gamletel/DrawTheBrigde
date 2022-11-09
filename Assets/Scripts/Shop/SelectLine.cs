a1a32b87f626bffde5441305a6c419427814f35e
Gamletel <gamletel@list.ru>
2022-11-09 17:08:34 +0300

    fix

--- Assets/Scripts/Shop/SelectLine.cs
+++ Assets/Scripts/Shop/SelectLine.cs
@@ -7,14 +7,14 @@ public class SelectLine : MonoBehaviour
 
     public void SetLine()
     {
-        if (PlayerPrefs.GetInt($"line{_id}Rewarded") == 0)
+        if (PlayerPrefs.GetInt($"Line{_id}Rewarded") == 0)
         {
             YandexSDK.instance.OnRewardedOpen(1);
             YandexSDK.instance.ShowRewarded("1");
             YandexSDK.instance.onRewardedAdReward += Instance_onRewardedAdReward;
         }
-        if (PlayerPrefs.GetInt($"line{_id}Rewarded") == 1)
-            PlayerPrefs.SetInt("line", _id);
+        if (PlayerPrefs.GetInt($"Line{_id}Rewarded") == 1)
+            PlayerPrefs.SetInt("Line", _id);
     }
 
     private void OnDisable()
@@ -24,7 +24,7 @@ public class SelectLine : MonoBehaviour
 
     private void Instance_onRewardedAdReward(string obj)
     {
-        PlayerPrefs.SetInt($"line{_id}Rewarded", 1);
-        PlayerPrefs.SetInt("line", _id);
+        PlayerPrefs.SetInt($"Line{_id}Rewarded", 1);
+        PlayerPrefs.SetInt("Line", _id);
     }
 }
--- Assets/Scripts/Tutorial/Level1Tutorial.cs
+++ Assets/Scripts/Tutorial/Level1Tutorial.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class Level1Tutorial : MonoBehaviour
 {
     private Animator _animator;
+    private bool _firstStep;
 
     private void Start() => _animator = GetComponent<Animator>();
 
     private void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && !_firstStep)
+        {
             _animator.SetInteger("tutorialIndex", 1);
+            _firstStep = true;
+        }
     }
 
     public void ExitTutorial()
